f6414b62e856848a7e8004ef4f6b889f9e3762cc
Tanvin5 <protten6@gmail.com>
2025-05-12 20:06:42 +0600

    Update admin.js

--- models/commands/admin.js
+++ models/commands/admin.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "admin",
   version: "1.0.5",
   hasPermssion: 0,
-  credits: "JOY",
+  credits: "Tanvin",
   description: "Admin Config",
   commandCategory: "Admin",
   usages: "Admin",
